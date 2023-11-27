413f8d57b049e6201aeb3a968e2f8cb0d50216e8
Emmanuelle BONOLI <manu.bonoli@gmail.com>
2023-11-27 09:49:24 +0100

    1ers tests

--- app.js
+++ app.js
@@ -1,43 +1,57 @@
-const express = require('express');
+const express = require("express");
 
 const app = express();
 const port = 5000;
 
+const movies = [
+  {
+    id: 1,
+    title: "Citizen Kane",
+    director: "Orson Wells",
+    year: "1941",
+    color: false,
+    duration: 120,
+  },
+  {
+    
+    id: 2,
+    title: "The Godfather",
+    director: "Francis Ford Coppola",
+    year: "1972",
+    color: true,
+    duration: 180,
+  },
+  {
+    id: 3,
+    title: "Pulp Fiction",
+    director: "Quentin Tarantino",
+    year: "1994",
+    color: true,
+    duration: 180,
+  },
+];
+
 app.get("/", (req, res) => {
-    res.send("Welcome to Express");
-  });
+    res.send("Welcome to my favourite movie list");
+});
 
-  const welcomeName = (req, res) => {
-    res.send(`Welcome ${req.params.name}`);
-  };
-  
-  app.get("/users/:name", welcomeName);
 
-  const cocktails = [
-    {
-      id: 1,
-      name: "Margarita",
-    },
-    {
-      id: 2,
-      name: "Mojito",
-    },
-    {
-      id: 3,
-      name: "Cuba Libre",
-    },
-  ];
+const getMovies = (req, res) => {
+    res.status(200).json(movies);
+};
   
-  const getCocktails = (req, res) => {
-    res.status(200).json(cocktails);
-  };
-  
-  app.get("/api/cocktails", getCocktails);
+app.get("/api/movies", getMovies);
 
-app.listen(port, (err) => {
-    if (err) {
-      console.error('Something bad happened');
-    } else {
-      console.log(`Server is listening on ${port}`);
+const getAMovie = (req, res) => {
+    for(let i=0; i<movies.length; i++){
+        if (req.params.id == movies[i].id){
+            return res.status(200).json(movies[i]);
+        } 
     }
-  });
+    return res.status(404).send('Not Found');
+};
+
+app.get("/api/movies/:id", getAMovie);
+
+
+module.exports = app;
